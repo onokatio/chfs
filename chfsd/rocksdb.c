76c7a9c5180cbe87f1b73fd872457bc861633357
onokatio <onokatio@gmail.com>
2023-11-02 13:19:59 +0900

    Increase stats dump frequency to 2 seconds, set create_if_missing option
    This change sets the stats_dump_period_sec option to 2 seconds in the
    rocksdb_options. Additionally, it sets the create_if_missing option to
    1.

--- chfsd/rocksdb.c
+++ chfsd/rocksdb.c
@@ -33,7 +33,8 @@ kv_init(char *db_dir, char *engine, char *path, size_t size)
 	//rocksdb_options_enable_statistics(options);
 	//rocksdb_options_set_statistics_level(options,
 	//				     rocksdb_statistics_level_all);
-	rocksdb_options_set_create_if_missing(options, 1);
+	rocksdb_options_set_stats_dump_period_sec(options, 2);
+    rocksdb_options_set_create_if_missing(options, 1);
 
 	char *p;
 	struct stat sb;
