a2498feb1bfc55b29e3787c567299e4099dbd2f0
Osamu Tatebe <tatebe@cs.tsukuba.ac.jp>
2024-08-24 23:09:37 +0900

    (dev) mpi test programs - sequel to cd4ee76b2dc0e0cb5f2a0eb2a8c28742e72be741

--- dev/mpi-test/open.c
+++ dev/mpi-test/open.c
@@ -9,7 +9,8 @@ main(int argc, char *argv[])
 	MPI_File fh;
 
 	_(MPI_Init(&argc, &argv));
-	_(MPI_File_open(MPI_COMM_WORLD, "chfs:test.txt", MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, &fh));
+	_(MPI_File_open(MPI_COMM_WORLD, "chfs:test.txt",
+			MPI_MODE_CREATE|MPI_MODE_WRONLY, MPI_INFO_NULL, &fh));
 	_(MPI_File_close(&fh));
 	_(MPI_Finalize());
 
